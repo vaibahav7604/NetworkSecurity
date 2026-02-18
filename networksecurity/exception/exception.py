f8f59391f3abcfad0d51884e460953e4641272ee
Meghani Vaibhav <vaibhavmeghani79@gmail.com>
2026-02-18 15:10:59 +0530

    data

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,20 +1,17 @@
-import sys 
+import sys
 from networksecurity.logging import logger
 
-
 class NetworkSecurityException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        self.error_message=error_message
-        _,_,exc_tb=error_detail.exc_info()
-
+    def __init__(self,error_message,error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+        
         self.lineno=exc_tb.tb_lineno
-        self.filename=exc_tb.tb_frame.f_code.co_filename
-
-
+        self.file_name=exc_tb.tb_frame.f_code.co_filename 
+    
     def __str__(self):
-        return f"Error occured in script [{self.filename}] at line number [{self.lineno}] error message [{self.error_message}]"
-        self.filename, self.lineno, str(self.error_message)
-
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        self.file_name, self.lineno, str(self.error_message))
 # if __name__=="__main__":
 #     try:
 #         logger.logging.info("This is an info message")
